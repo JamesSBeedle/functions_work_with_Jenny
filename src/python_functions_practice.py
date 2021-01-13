1434efb986489e2aeb7524a554fdfb423e84be82
JamesSBeedle <jamessbeedle@gmail.com>
2021-01-13 12:07:08 +0000

    first 2 tests passing

---
+++ src/python_functions_practice.py
@@ -0,0 +1,14 @@
+def return_10():
+    return 10
+
+
+def add(number_1, number_2):
+    return number_1 + number_2
+
+
+
+def subtract(number_1, number_2):
+    return number_1 - number_2
+
+subtract(10, 5)
+
